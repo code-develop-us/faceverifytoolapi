54a6667b2dcc653b8d8695d03da81723bcb53ada
code-develop-us <code.develop.us@gmail.com>
2024-06-13 20:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,20 @@ app =Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def main():
-    # img1Path=request.json['img1Path']
-    # img2Base64String=request.json['img2Base64']
-    # if 'data:image' in img2Base64String:
-    #     img2Base64String=img2Base64String.split(',')[1]
-    # imgBytes=base64.b64decode(img2Base64String)
-    # imageStream=BytesIO(imgBytes)
-    # image=Image.open(imageStream)
-    # # img1Path='env\IMG_20240612_205619.jpg'
-    # # image=Image.open('env\IMG_20240612_205627.jpg')  
-    # imgArray=asarray(image)
+    img1Path=request.json['img1Path']
+    img2Base64String=request.json['img2Base64']
+    if 'data:image' in img2Base64String:
+        img2Base64String=img2Base64String.split(',')[1]
+    imgBytes=base64.b64decode(img2Base64String)
+    imageStream=BytesIO(imgBytes)
+    image=Image.open(imageStream)
+    # img1Path='env\IMG_20240612_205619.jpg'
+    # image=Image.open('env\IMG_20240612_205627.jpg')  
+    imgArray=asarray(image)
 
-    # result = DeepFace.verify(img1_path=img1Path,img2_path=imgArray)
+    result = DeepFace.verify(img1_path=img1Path,img2_path=imgArray)
     # print(json.dumps(result,indent=2))
-    return jsonify({'result':'true'})
+    return jsonify(result)
 
 if __name__=='__main__':
     app.run()
